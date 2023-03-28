793eb18ebcfbc2e4f82f00eb7c9145025652a954
Krasimir Nenov <115361185+nenovKrasimir@users.noreply.github.com>
2023-03-28 23:44:37 +0200

    added new endpoint verify email

--- resources/routes.py
+++ resources/routes.py
@@ -1,4 +1,4 @@
-from resources.users import UserRegister, UserLogin
+from resources.users import UserRegister, UserLogin, VerifyUser
 from resources.admin_panel import AddCoach
 from resources.refresh_token import RefreshToken
 
@@ -6,5 +6,6 @@ routes = (
     (UserRegister, "/registration"),
     (UserLogin, "/login"),
     (AddCoach, "/admin_panel"),
-    (RefreshToken, "/refresh_token")
+    (RefreshToken, "/refresh_token"),
+    (VerifyUser, "/verify_email/<token>")
 )
