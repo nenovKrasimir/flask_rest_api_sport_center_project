8b91b6c34486dfeb949db4b561e992abc2b1e8b0
Krasimir Nenov <115361185+nenovKrasimir@users.noreply.github.com>
2023-03-28 23:44:01 +0200

    added verify user logic and email integration

--- managers/user_manager.py
+++ managers/user_manager.py
@@ -1,8 +1,9 @@
-from werkzeug.security import generate_password_hash, check_password_hash
+import boto3
 from werkzeug.exceptions import BadRequest
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from db import db
-from managers.auth_manager import TokenManger
+from managers.auth_manager import TokenManger, get_authentication
 from models.user_register import AllUsers
 
 
@@ -13,6 +14,47 @@ class RegisterUser:
         user = AllUsers(**data)
         db.session.add(user)
         db.session.commit()
+        email_token = TokenManger.encode_email_verify_token(user)
+        ses = boto3.client(
+            'ses',
+            region_name='eu-north-1',
+            aws_access_key_id='AKIAWWUKQSYTQPB66D7B',
+            aws_secret_access_key='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'
+        )
+        # Extract the necessary information from the request data
+        sender = 'k.nenov96@abv.bg'
+        recipient = data['email']
+        subject = 'Registration Successful'
+        message = 'Thank you for registering!' \
+                  f'Link for verifying email: http://127.0.0.1:5000/verify_email/{email_token}'
+
+        # Send an email to the new user using the SES client
+        response = ses.send_email(
+            Source=sender,
+            Destination={
+                'ToAddresses': [recipient]
+            },
+            Message={
+                'Subject': {'Data': subject},
+                'Body': {'Text': {'Data': message}}
+            }
+        )
+
+    @staticmethod
+    def verify_user(token):
+        user_data = TokenManger.decode_email_verify_token(token)
+        user = AllUsers.query.filter_by(email=user_data['email']).first()
+        if not user:
+            raise BadRequest("Not a valid page")
+
+        if not user.verified:
+            user.verified = True
+            db.session.commit()
+            return {"Success": "Your account is verified you can login now!"}, 200
+
+        if user.verified:
+            raise BadRequest("Your account is already verified, you can log in")
+
 
 
 class LoginUser:
@@ -22,5 +64,8 @@ class LoginUser:
         user = AllUsers.query.filter_by(username=username).first()
         if not check_password_hash(user.password, password):
             raise BadRequest("Invalid username or password")
+        if not user.verified:
+            raise BadRequest("Please verify your email in order to login!")
+
         refresh_token, access_token = TokenManger.encode_refresh_token(user), TokenManger.encode_access_token(user)
         return {"refresh_token": refresh_token, "access_token": access_token}
