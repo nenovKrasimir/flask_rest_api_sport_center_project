a9d4e993b3a91f79ed79ae8dc4b89e207d1a775f
Krasimir Nenov <115361185+nenovKrasimir@users.noreply.github.com>
2023-04-16 17:53:51 +0200

    added stripe payment provider logic

---
+++ services/payment_provider_service_stripe.py
@@ -0,0 +1,67 @@
+import stripe
+from werkzeug.exceptions import BadRequest
+
+
+class StripePaymentService:
+    def __init__(self, api_key):
+        stripe.api_key = api_key
+        self.payment = None
+
+    @staticmethod
+    def create_subscription(customer_id, subscription_id):
+        try:
+            subscription = stripe.Subscription.create(
+                customer=customer_id,
+                items=[{
+                    'price': subscription_id,
+                }],
+                payment_behavior='default_incomplete',
+                expand=['latest_invoice.payment_intent']
+            )
+
+            return subscription['id']
+
+        except (stripe.error.CardError,
+                stripe.error.RateLimitError,
+                stripe.error.InvalidRequestError,
+                stripe.error.AuthenticationError,
+                stripe.error.APIConnectionError,
+                stripe.error.StripeError) as e:
+            raise BadRequest(f"{e}")
+
+    @staticmethod
+    def create_customer(email, name, source):
+        try:
+            customer = stripe.Customer.create(
+                email=email,
+                name=name,
+                source=source,
+            )
+
+            return customer['id']
+
+        except (stripe.error.RateLimitError,
+                stripe.error.InvalidRequestError,
+                stripe.error.AuthenticationError,
+                stripe.error.APIConnectionError,
+                stripe.error.StripeError) as e:
+            raise BadRequest(f"{e}")
+
+    @staticmethod
+    def buy_equipments(customer_id, equipment_amount):
+        try:
+            payment_intent = stripe.PaymentIntent.create(
+                amount=equipment_amount,
+                currency="BGN",
+                customer=customer_id,
+                payment_method="pm_card_visa",
+            )
+
+            payment_intent.confirm()
+
+        except (stripe.error.RateLimitError,
+                stripe.error.InvalidRequestError,
+                stripe.error.AuthenticationError,
+                stripe.error.APIConnectionError,
+                stripe.error.StripeError) as e:
+            raise BadRequest(f"{e}")
