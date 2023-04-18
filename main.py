57cbc6576f4fd3b66d11c72e2158e8038d5f9db4
Krasimir Nenov <115361185+nenovKrasimir@users.noreply.github.com>
2023-04-18 22:30:04 +0200

    added basic task with schedule

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
+import threading
+import time
+
+import schedule
 from dotenv import dotenv_values
 from flask import Flask
 from flask_migrate import Migrate
 from flask_restful import Api
 
 from db import db
+from managers.admin_access_managers.delivery_manager import DeliveryGuyManger
 from resources.routes import routes
 
 config = {**dotenv_values(".env")}
@@ -17,6 +22,17 @@ migrate = Migrate(app, db)
 
 [api.add_resource(*route) for route in routes]
 
+schedule.every(5).seconds.do(DeliveryGuyManger.move_delivered_packages)
+
+
+def run_schedule():
+    with app.app_context():
+        while True:
+            schedule.run_pending()
+            time.sleep(1)
+
 
 if __name__ == "__main__":
+    schedule_thread = threading.Thread(target=run_schedule)
+    schedule_thread.start()
     app.run()
